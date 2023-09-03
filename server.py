1c3c7cb17f0a2eb74c891f539dbc086752e0b6a8
drlifetech <44934951+drlifetech@users.noreply.github.com>
2023-09-03 00:33:03 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,30 @@
+import sqlite3
+import hashlib
+import socket
+import threading
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("drlife.pythonanywhere.com", 9999))
+
+server.listen()
+
+def handle_connection(c):
+    hwid = c.recv(1024).decode()
+    key = c.recv(1024)
+    key = hashlib.sha256(key).hexdigest()
+
+    con = sqlite3.connect("userdata.db")
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM userdata WHERE hwid = ? AND key = ?", (hwid, key))
+
+    if cur.fetchall():
+        c.send(b"yes")
+        print("yes")
+    else:
+        c.send(b"no")
+        print("no")
+
+while True:
+    client, addr = server.accept()
+    threading.Thread(target=handle_connection, args=(client,)).start()
